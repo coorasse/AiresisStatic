ee3b1efa390ece47c14286e6830fbd11e9a6cd7c
Alessandro Rodi <alessandro.rodi@renuo.ch>
2015-05-22 08:29:17 +0200

    new languages, switch to php

---
+++ index.php
@@ -0,0 +1,186 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Airesis | Welcome</title>
+    <link rel="stylesheet" href="css/foundation.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
+    <script src="js/vendor/modernizr.js"></script>
+</head>
+<body>
+
+<div class="row">
+    <div class="large-12 columns text-center">
+        <img alt="Airesis - Scegli di partecipare" src="./img/logo_completo_medium.png"
+             title="Airesis - Make a decision">
+
+        <h1 class="responsive-headline" style="font-size: 50px;">The Social Network for E-Democracy</h1>
+    </div>
+</div>
+
+<div class="row text-center">
+    <div class="columns">
+        <h1>EUROPE</h1>
+    </div>
+</div>
+
+<div class="row text-center">
+    <div class="columns">
+        <ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-4">
+            <?php $url = "https://www.airesis.eu/" ?>
+            <?php $country = "Europe" ?>
+            <?php $flag = "en" ?>
+            <?php include('lang.php') ?>
+
+            <?php $url = "https://www.airesis.fr/" ?>
+            <?php $country = "France" ?>
+            <?php $flag = "fr" ?>
+            <?php include('lang.php') ?>
+
+            <?php $url = "https://www.airesis.eu?l=hu" ?>
+            <?php $country = "Hungary" ?>
+            <?php $flag = "hu" ?>
+            <?php include('lang.php') ?>
+
+            <?php $url = "https://www.airesis.it" ?>
+            <?php $country = "Italia" ?>
+            <?php $flag = "it-IT" ?>
+            <?php include('lang.php') ?>
+
+            <?php $url = "https://www.airesis.de" ?>
+            <?php $country = "Deutschland" ?>
+            <?php $flag = "de" ?>
+            <?php include('lang.php') ?>
+
+            <?php $url = "https://www.airesis.eu?l=ro-RO" ?>
+            <?php $country = "România" ?>
+            <?php $flag = "ro-RO" ?>
+            <?php include('lang.php') ?>
+
+            <?php $url = "https://www.airesis.eu?l=es-ES" ?>
+            <?php $country = "España" ?>
+            <?php $flag = "es-ES" ?>
+            <?php include('lang.php') ?>
+
+            <?php $url = "https://www.airesis.eu?l=pt-PT" ?>
+            <?php $country = "Portugal" ?>
+            <?php $flag = "pt-PT" ?>
+            <?php include('lang.php') ?>
+
+            <?php $url = "https://www.airesis.eu?l=el" ?>
+            <?php $country = "Greece" ?>
+            <?php $flag = "el" ?>
+            <?php include('lang.php') ?>
+
+            <?php $url = "https://www.airesis.eu?l=en-GB" ?>
+            <?php $country = "United Kingdom" ?>
+            <?php $flag = "en-GB" ?>
+            <?php include('lang.php') ?>
+        </ul>
+    </div>
+</div>
+
+<div class="row text-center">
+    <div class="columns">
+        <h1>AMERICAS</h1>
+    </div>
+</div>
+
+<div class="row text-center">
+    <div class="columns">
+        <ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-4">
+            <?php $url = "https://www.airesis.us" ?>
+            <?php $country = "USA" ?>
+            <?php $flag = "en-US" ?>
+            <?php include('lang.php') ?>
+
+            <?php $url = "https://www.airesis.us?l=pt-BR" ?>
+            <?php $country = "Brasil" ?>
+            <?php $flag = "pt-BR" ?>
+            <?php include('lang.php') ?>
+
+            <?php $url = "https://www.airesis.us?l=es-EC" ?>
+            <?php $country = "Ecuador" ?>
+            <?php $flag = "es-EC" ?>
+            <?php include('lang.php') ?>
+
+            <?php $url = "https://www.airesis.us?l=es-CL" ?>
+            <?php $country = "Chile" ?>
+            <?php $flag = "es-CL" ?>
+            <?php include('lang.php') ?>
+        </ul>
+
+    </div>
+</div>
+
+
+<div class="row text-center">
+    <div class="columns">
+        <h1>ASIA</h1>
+    </div>
+</div>
+
+<div class="row text-center">
+    <div class="columns">
+        <ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-4">
+            <?php $url = "https://www.airesis.ru" ?>
+            <?php $country = "Russia" ?>
+            <?php $flag = "ru" ?>
+            <?php include('lang.php') ?>
+
+            <?php $url = "https://www.airesis.cn" ?>
+            <?php $country = "中国" ?>
+            <?php $flag = "zh" ?>
+            <?php include('lang.php') ?>
+        </ul>
+    </div>
+</div>
+
+<div class="row text-center">
+    <div class="columns">
+        <h1>AFRICA</h1>
+    </div>
+</div>
+
+<div class="row text-center">
+    <div class="columns">
+        <ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-4">
+            <?php $url = "https://www.airesis.eu?l=en-ZA" ?>
+            <?php $country = "South Africa" ?>
+            <?php $flag = "en-ZA" ?>
+            <?php include('lang.php') ?>
+
+        </ul>
+    </div>
+</div>
+
+<div class="row text-center">
+    <div class="columns">
+        <h1>OCEANIA</h1>
+    </div>
+</div>
+
+<div class="row text-center">
+    <div class="columns">
+        <ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-4">
+            <?php $url = "https://www.airesis.eu?l=en-AU" ?>
+            <?php $country = "Australia" ?>
+            <?php $flag = "en-AU" ?>
+            <?php include('lang.php') ?>
+
+            <?php $url = "https://www.airesis.eu?l=en-NZ" ?>
+            <?php $country = "New Zealand" ?>
+            <?php $flag = "en-NZ" ?>
+            <?php include('lang.php') ?>
+        </ul>
+    </div>
+</div>
+
+<script src="js/vendor/jquery.js"></script>
+<script src="js/foundation.min.js"></script>
+<script>
+    $(document).foundation();
+</script>
+</body>
+</html>
---
+++ lang.php
@@ -0,0 +1,5 @@
+<li>
+    <a href="<?php echo $url ?>" title="<?php echo $country ?>">
+        <img alt="<?php echo $country ?>" src="./img/flags/<?php echo $flag ?>.png" class="flag">
+    </a>
+</li>
